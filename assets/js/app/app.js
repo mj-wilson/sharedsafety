e3de92f330a43ffc58be6a94cee4a394631cbd31
Marisa Wilson <marisajune@gmail.com>
2017-03-02 10:29:28 -0800

    principle page: close and scroll to section when target is on current page

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,16 +1,19 @@
 jQuery(function( $ ) {
 	
- if ( $( 'body.page-template-template-practice' ).length ) {
+  // scroll to top of practice page
+  if ( $( 'body.page-template-template-practice' ).length ) {
     $( '.page-header' ).click( function () {
     		$( 'html, body' ).animate({ scrollTop: 0 }, "fast" );
   	});	
   }
 
+  // open/close side nav
 	$( '.nav-toggle-button' ).click( function () {
   		$( this ).parent().toggleClass( 'open' );
 
 	});	
-
+  
+  // initiate home page slider
 	$( '.slides_holder' ).slick({
 	  	prevArrow: '#slick_arrow_left',
 	  	nextArrow: '#slick_arrow_right',
@@ -18,14 +21,16 @@ jQuery(function( $ ) {
       infinite: false,
       dots: true
 	});
-
+  
+  // initiate global scroll to section functionality
 	$( '.link_to_section' ).click( function () {
   	var target = '#' + $( this ).attr( 'data-target' );
 		$( 'html,body' ).animate({
 		   scrollTop: $( target ).offset().top
 		});
 	});	
-  
+
+  // principle page: highlight active panel  
   if ( $( '.page-template-template-practice' ).length ) {
       $(window).on( "load resize scroll" , function( e ) {
       var scrolledPastIntro = $( '.step_panel' ).offset().top - 10;
@@ -48,6 +53,7 @@ jQuery(function( $ ) {
   
   }
 	
+  // principle page: open and close example modal  
   $( '.open_modal' ).click( function () {
   		$( this ).next( '.modal' ).show();
       $( 'body' ).addClass( 'modal_open' );
@@ -56,25 +62,48 @@ jQuery(function( $ ) {
   		$( '.modal' ).hide();
       $( 'body' ).removeClass( 'modal_open' );
 	});	
+
+  // principle page: open and close grid overview  
   $( '.open-bp-overview' ).click( function () {
       $( '.grid-overlay' ).addClass( 'open' );
   }); 
   $( '.close-overlay' ).click( function () {
       $( '.grid-overlay' ).removeClass( 'open' );
   }); 
-  
-  
+
+  // principle page: close and scroll to section when target is on current page  
+  $( '.blueprint_grid a' ).click(function(){
+    var target = $( this )[0].pathname;
+    var pathname = window.location.pathname;
+    if ( target == pathname) {
+      event.preventDefault();
+      var hash = this.hash.substr(1);
+      if ( hash ) {
+        hash = '#' + hash;
+        $( 'html,body' ).animate({
+           scrollTop: $( hash ).offset().top
+        });
+      } else {
+          $( 'html, body' ).animate({ scrollTop: 0 });
+      }
+      $( '.grid-overlay' ).removeClass( 'open' );
+    }
+  });
+
+  // principle page: reverse animation when going backwards
   if ( document.URL.indexOf( 'bw=1' ) !== -1 )  {
     $( '.page-wrapper' ).removeClass( 'scene_element--fadeinright' ).addClass( 'scene_element--fadeinleft' );
   }
 
+  // contact page: validate email submission form & show thanks after submission
   if ( $( 'body.page-template-template-contact' ).length ) {
     $( '#headerform' ).validate();
   }
   if ( document.URL.indexOf( 'key=' ) !== -1 )  {
     $( '.signup_form_thanks' ).show();
   }
-  
+
+  // contact page: show breadcrumb if clicking from principle & set link href
   $( '.breadcrumb' ).toggle( document.URL.indexOf( 'principle=' ) !== -1 );
 
   if ( document.URL.indexOf( 'principle=' ) !== -1 )  {
@@ -87,6 +116,8 @@ jQuery(function( $ ) {
     $( '.breadcrumb a' ).attr( 'href', target );
 
   }
+
+  // principle page: remove animation triggering classes to prevent fixed
   setTimeout(function(){ 
     $( '.page-wrapper' ).removeClass( 'scene_element--fadeinright' ).removeClass( 'scene_element--fadeinleft' );
   }, 3000);
